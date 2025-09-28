e71c78f4009349f03b344909636a1bf16bc95e35
Abhishek-1337 <abhishekrinku4@gmail.com>
2025-09-28 13:52:53 +0530

    websocket server token authentication

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,29 @@
 import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+const JWT_SECRET = "Abhishek2000"
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', async function connection(ws, request) {
+  const url = request.url;
+  if(!url) {
+    ws.close();
+    return;
+  }
+
+  const params = new URLSearchParams(url?.split("?")[1]);
+  const token = params.get("token");
+  if(!token){
+    ws.close();
+    return;
+  }
+
+  const decoded = await jwt.verify(token, JWT_SECRET);
+  if(!decoded || !(decoded as JwtPayload).userId){
+    ws.close();
+    return;
+  }
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
